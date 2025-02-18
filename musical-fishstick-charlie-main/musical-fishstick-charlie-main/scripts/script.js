e72e46ef18933bad6d256de902781e3b3e1f29d1
Olivia-Sharpston <osharpston@una.edu>
2025-02-18 11:59:56 -0600

    Add files via upload

--- musical-fishstick-charlie-main/musical-fishstick-charlie-main/scripts/script.js
+++ musical-fishstick-charlie-main/musical-fishstick-charlie-main/scripts/script.js
@@ -1,4 +1,20 @@
 // some js
 $( document ).ready(function() {
-    console.log( "hey blah blah" );
+    console.log( "hey dr. cumbie, don't hover the seals...or do" );
+
+    $('#secretSeal').hover(
+
+        function(){
+            $(this).css("background-color", "pink");
+            // alert('never use alert');
+            }
+    ); 
+    // $('#sillySeal').hover(
+
+    //     function(){
+    //         $(this).css("transform", "rotate(45deg)");
+    //         $("#sillySeal").slideDown();
+    //         $(this).css("background-color", "pink");
+    //        }
+    // ); 
 });
