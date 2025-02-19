73949717e26b8858ba98739a7922f9be38d6e8ea
Olivia-Sharpston <osharpston@una.edu>
2025-02-19 15:47:51 -0600

    Add files via upload

--- devC/scripts/script.js
+++ devC/scripts/script.js
@@ -1,12 +1,12 @@
 // some js
 $( document ).ready(function() {
-    console.log( "hey dr. cumbie, don't hover the seals...or do" );
+    console.log( "hey dr. cumbie, don't hover the seal...or do" );
+    console.log( "also, maybe home will take you home?" );
 
     $('#secretSeal').hover(
 
         function(){
             $(this).css("background-color", "pink");
-            // alert('never use alert');
             }
     ); 
 });
